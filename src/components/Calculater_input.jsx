abdcf887e878170747e715921f7d8ab9eb6a3d74
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-02-19 16:27:07 +0530

    Update: set defalut Values

--- src/components/Calculater_input.jsx
+++ src/components/Calculater_input.jsx
@@ -1,50 +1,77 @@
-import {calculateInvestmentResults } from '../util/investment'
-import { useState } from "react"
-function Calculator_input(){
-   const [initial, setInitial]=useState("")
-   const [anual, setAnual]=useState("")
-   const [expectedreturn, setExpectedreturn]=useState("")
-   const [duration, setDuration]=useState("")
-   function handleInitialChange(event){
-    setInitial(event.target.value)
-   }
-   function handleAnualChange(event){
-    setAnual(event.target.value)
-   }
-   function handleReturnChange(event){
-    setExpectedreturn(event.target.value)
-   }
-   function handleDurationChange(event){
-    setDuration(event.target.value)
-   }
-   console.log(initial, anual, expectedreturn, duration)
-   const calculatedAnnualData  = calculateInvestmentResults({initial, anual, expectedreturn, duration})
-//    getValue(calculatedAnnualData)
-   console.log(calculatedAnnualData)
-    return (
-        <div id='user-input'>
-            <div className='input-group'>
-            <p>
-            <label id = "iv">Initial Investment</label>
-            <input id ="iv" type="number" onChange={handleInitialChange} value={initial} required/>
-            </p>
-            <p>
-            <label id = "an">Anumal Investment</label>
-            <input id ="an" type="number" onChange={handleAnualChange} value={anual} required/>
-            </p>
-            </div>
-            <div className='input-group'>
-                <p>
-            <label id = "er">Expected Return</label>
-            <input id ="er" type="number" onChange={handleReturnChange} value={expectedreturn} required/>
-            </p>
-            <p>
-            <label id = "D">Duration</label>
-            <input id ="D" type="number" onChange={handleDurationChange} value={duration} required/>
-            </p>
-            </div>
-        </div>
-    )
+import { calculateInvestmentResults } from "../util/investment";
+import { useState } from "react";
+function Calculator_input() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1500,
+    expectedReturn: 10,
+    duration: 10,
+  });
+  function handelChange(initialIdentifiyer, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...userInput,
+        [initialIdentifiyer]: newValue,
+      };
+    });
+  }
+
+
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            onChange={(event) =>
+              handelChange("initialInvestment", event.target.value)
+            }
+            value={userInput.initialInvestment}
+            required
+          />
+        </p>
+        <p>
+          <label id="an">Anumal Investment</label>
+          <input
+            id="an"
+            type="number"
+            onChange={(event) =>
+                handelChange("annualInvestment", event.target.value)
+              }
+            value={userInput.annualInvestment}
+            required
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label id="er">Expected Return</label>
+          <input
+            id="er"
+            type="number"
+            onChange={(event) =>
+                handelChange("expectedReturn", event.target.value)
+              }
+            value={userInput.expectedReturn}
+            required
+          />
+        </p>
+        <p>
+          <label id="D">Duration</label>
+          <input
+            id="D"
+            type="number"
+            onChange={(event) =>
+                handelChange("duration", event.target.value)
+              }
+            value={userInput.duration}
+            required
+          />
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Calculator_input ;
+export default Calculator_input;
